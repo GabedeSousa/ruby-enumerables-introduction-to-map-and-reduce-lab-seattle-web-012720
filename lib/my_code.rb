7134d6424761029d6af010f4986d397d82a2f1e6
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-15 00:48:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,8 +32,11 @@ def map_to_square(source_array)
   new_array = []
   i = 0 
   while i < source_array.length do 
-    new_array.push
-
-end 
+    new_array.push(source_array[i] ** 2)
+    i += 1 
+    end
+    new_array
+  end
+  
 
   
