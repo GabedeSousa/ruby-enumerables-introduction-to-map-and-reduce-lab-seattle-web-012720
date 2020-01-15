312b38eda7fdcae3c3ad407882603001f70301c0
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-15 00:59:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,5 +58,12 @@ return true
 end
 
 def reduce_to_any_true(source_array)
-  
+  i = 0 
+  while i < source_array.length do
+    return true if source_array[i] == true
+    i += 1 
+end 
+return false 
+end
+
   
